27a3ac92c39fa1e6ad4daaa1fae089a8184e749f
CICSilver <44863433+CICSilver@users.noreply.github.com>
2018-12-07 11:57:40 +0800

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -105,12 +105,66 @@ public class Solution {
         }
     return t;
     }
+    //求两个数组的交集
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if(nums1.length==0||nums2.length==0) return null;
+        int count=0;
+        nums1=quickSort(nums1);
+        nums2=quickSort(nums2);
+        if(nums1.length>nums2.length) count=nums2.length;
+        else count=nums1.length;
+        int temp[]=new int[count];
+        int N=0;
+        for(int i=0;i<nums1.length;i++) {
+            for(int j=0;j<nums2.length;j++) {
+                boolean flag=false;
+                /*if(j!=nums2.length-1) {
+                    if ((nums2[j] ^ nums2[j + 1]) == 0)
+                        continue;
+                }*/
+                if((nums1[i]^nums2[j])==0) {
+                    for (int t = 0; t < temp.length; t++) {
+                        if (temp[t] == nums1[i]) {
+                            flag = true;
+                            continue;
+                        }
+                    }
+                    if(flag == false)
+                        temp[N++] = nums1[i];
+                }
+                continue;
+            }
+        }
+        temp=removeZero(temp);
+        return temp;
+    }
+    private int[] removeZero(int[] nums) {
+        int[] temp;
+        int count=0;
+        for(int i=0;i<nums.length;i++) {
+            if(nums[i]!=0)
+                count++;
+        }
+        temp=new int[count];
+        for(int i=0;i<nums.length;i++) {
+            if(nums[i]!=0)
+                temp[i]=nums[i];
+        }
+        return temp;
+    }
+    public boolean isPowerOfThree(int n) {
+        if(n==0) return false;
+        boolean flag=false;
+        if(n%3==0)
+            flag=true;
+        return flag;
+
+    }
     public static void main(String[] args) {
-        int[] temp={1};
+        int n=10;
         Solution so=new Solution();
-        Boolean flag=so.containsDuplicate(temp);
-        int t=so.singleNumber(temp);
-        System.out.println(t);
+        boolean flag= so.isPowerOfThree(n);
+        System.out.println(flag);
     }
 }
 
