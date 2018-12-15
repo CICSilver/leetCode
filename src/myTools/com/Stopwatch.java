dcf9233680be3baa374cacd03c49a46c7a14f6e6
CICSilver <44863433+CICSilver@users.noreply.github.com>
2018-12-15 17:59:55 +0800

    添加了计时器

---
+++ src/myTools/com/Stopwatch.java
@@ -0,0 +1,14 @@
+package myTools.com;
+
+public class Stopwatch {
+    private double start=0;
+    Stopwatch() {
+        start=System.currentTimeMillis();
+    }
+
+    public double elapsedTime() {
+        double endTime=System.currentTimeMillis();
+        return (endTime-start)/1000;
+    }
+
+}
