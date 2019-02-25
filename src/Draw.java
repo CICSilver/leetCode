b67e51e04ae832eee4d419f5c70ef1372b787c00
CICSilver <1038516368@qq.com>
2019-02-25 19:31:46 +0800

    添加可视化数组

--- src/Draw.java
+++ src/Draw.java
@@ -1,19 +1,27 @@
 import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdRandom;
 
-import javax.swing.*;
-import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Draw extends JFrame {
-    public Draw() {
-        super("Draw");
-        Dimension dimension=getToolkit().getScreenSize();
-        //获取屏幕分辨率
-        setBounds(dimension.width/4,dimension.height/4,dimension.width/2,dimension.height/2);
-        StdDraw.point(108,120);
-        this.setVisible(true);
-    }
-    public static void main(String[] args) {
-        Draw draw=new Draw();
+
+public class Draw{
+    public static void main(String[] args){
+        int len=50;
+        StdDraw.setCanvasSize(1024 ,1024);
+        double[] array=new double[len];
+        for(int i=0;i<len;i++) {
+            array[i]= StdRandom.uniform();
+        }
+        StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE);
+        for(int i=0;i<len;i++) {
+            double y= array[i]/2.0;
+            double width=0.5 / len;
+            double x= 1.0 * i / len+width;
+            double height=array[i]/2.0;
+            StdDraw.filledRectangle(x,y,width-0.003,height);
+
+        }
     }
 }
 
