b33e3d85945c9d6aba15547bb594deaa3e39505f
CICSilver <1038516368@qq.com>
2019-02-25 20:31:30 +0800

    使用List实现的排序可视化

--- src/Draw.java
+++ src/Draw.java
@@ -1,26 +1,36 @@
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdRandom;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 
 public class Draw{
+    private void drawRectangle(int i,int len,double num) {
+        double y=num/2.0;
+        double width=0.5/len;
+        double x=1.0*i/len+width;
+        //(x,y)为矩形条中心点坐标，width为矩形宽度的一般
+        StdDraw.filledRectangle(x,y,width-0.003,y);
+    }
     public static void main(String[] args){
         int len=50;
-        StdDraw.setCanvasSize(1024 ,1024);
-        double[] array=new double[len];
-        for(int i=0;i<len;i++) {
-            array[i]= StdRandom.uniform();
+        int i=0;
+        Draw draw=new Draw();
+        List<Double> list=new ArrayList<>();
+        for(;i<len;i++) {
+            list.add(StdRandom.uniform());
         }
-        StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE);
-        for(int i=0;i<len;i++) {
-            double y= array[i]/2.0;
-            double width=0.5 / len;
-            double x= 1.0 * i / len+width;
-            double height=array[i]/2.0;
-            StdDraw.filledRectangle(x,y,width-0.003,height);
-
+        for(i=0;i<len;i++) {
+            draw.drawRectangle(i,len,list.get(i));
+            StdDraw.pause(20);
+        }
+        StdDraw.pause(1000);
+        list.sort(Double::compareTo);
+        StdDraw.clear();
+        for(i=0;i<len;i++) {
+            draw.drawRectangle(i,len,list.get(i));
+            StdDraw.pause(20);
         }
     }
 }
