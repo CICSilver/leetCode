5a6acf0da2f134f4d84412906a503584bed16097
CICSilver <1038516368@qq.com>
2019-03-03 15:26:13 +0800

    更新单元测试的异常规避部分

--- src/myTools/tools_test.java
+++ src/myTools/tools_test.java
@@ -5,30 +5,36 @@ import myTools.List.MyArrayList;
 import java.util.Arrays;
 
 public class tools_test {
-    public void josef(MyArrayList arrayList) {
+    private void josef(MyArrayList arrayList,int startNum,int initCount) {
+        if(arrayList==null) {
+            return;
+        }
+        if(startNum<0||startNum>arrayList.size()) {
+            throw new IndexOutOfBoundsException("起始下标"+startNum+"超出集合范围");
+        }
         int count=1;
-        int initCount=4;
-        int nums=0;
         while(arrayList.size()!=1) {
             while(count!=initCount) {
                 count++;
             }
             count=1;
-            nums+=initCount-1;
-            while(nums>=arrayList.size()) {
-                nums=nums-arrayList.size();
+            startNum+=initCount-1;
+            while(startNum>=arrayList.size()) {
+                startNum=startNum-arrayList.size();
             }
-            arrayList.remove(nums);
+            arrayList.remove(startNum);
             System.out.println( Arrays.toString(arrayList.asArray()));
         }
     }
     public static void main(String[] args) {
         int len=10;
+        int initCount=4;
+        int startNum=0;
         MyArrayList<Integer> arrayList=new MyArrayList<>(10);
         for(int i=0;i<len;i++) {
             arrayList.add(i);
         }
         tools_test tools_test=new tools_test();
-        tools_test.josef(arrayList);
+        tools_test.josef(arrayList,startNum,initCount);
     }
 }
