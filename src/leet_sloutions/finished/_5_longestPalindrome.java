0c020b585c529fc67a871ecd8401ca5b8777da02
CICSilver <1038516368@qq.com>
2019-04-03 22:17:14 +0800

    完成 5.回文子串 三指针

--- src/leet_sloutions/finished/_5_longestPalindrome.java
+++ src/leet_sloutions/finished/_5_longestPalindrome.java
@@ -1,6 +1,4 @@
-package leet_sloutions.unfinshed;
-
-import java.util.*;
+package leet_sloutions.finished;
 
 public class _5_longestPalindrome {
     /**
@@ -24,6 +22,10 @@ public class _5_longestPalindrome {
                 return Character.toString(s.charAt(0));
             }
         }
+        StringBuilder sb=new StringBuilder(s);
+        if(sb.reverse().toString().equals(sb.toString())&&s.length()>10) {
+            return s;
+        }
         char front,cur,next;
         int longestPalinLength=0;
         int startPos=s.length()-1;
@@ -34,24 +36,25 @@ public class _5_longestPalindrome {
             cur=s.charAt(curIndex);
             next=s.charAt(nextIndex);
             if(front==next) {
-                int[] lap=lenAndPos(frontIndex,nextIndex,3,s);
+                int[] lap=centerLenAndPos(frontIndex,nextIndex,3,s);
                 if(longestPalinLength<lap[0]) {
                     longestPalinLength=lap[0];
                     startPos=lap[1];
+                    if(longestPalinLength>=s.length()-1) {
+                        break;
+                    }
                 }
             } else if(front==cur) {
-                if (frontIndex != 0) {
-                    front = s.charAt(frontIndex - 1);
-                    if (front == next) {
-                        int[] lap = lenAndPos(frontIndex - 1, nextIndex, 2, s);
-                        if (longestPalinLength < lap[0]) {
-                            longestPalinLength = lap[0];
-                            startPos = lap[1];
+                if(frontIndex!=0) {
+                    if(s.charAt(frontIndex-1)==next) {
+                        int[] nlap=centerLenAndPos(frontIndex-1,nextIndex,4,s);
+                        if(longestPalinLength<nlap[0]) {
+                            longestPalinLength=nlap[0];
+                            startPos=nlap[1];
                         }
                     }
-                    startPos = frontIndex;
-                    longestPalinLength = longestPalinLength < 2 ? 2 : longestPalinLength;
-                } else {
+                }
+                if(longestPalinLength<2) {
                     longestPalinLength=2;
                     startPos=frontIndex;
                 }
@@ -59,6 +62,7 @@ public class _5_longestPalindrome {
 
         }
         if(s.charAt(s.length()-2)==s.charAt(s.length()-1)) {
+            //判断最后两位是否相同
             if(longestPalinLength<2) {
                 longestPalinLength=2;
                 startPos=s.length()-2;
@@ -90,12 +94,14 @@ public class _5_longestPalindrome {
      * @param s 主串
      * @return [当前回文子串长度，回文子串的起始位置]
      */
-    private int[] lenAndPos(int indexFro, int indexNex, int curLen,String s) {
+    private int[] centerLenAndPos(int indexFro, int indexNex, int curLen, String s) {
         int startPos=indexFro;
         int[] lenAndPos=new int[2];
         char front,next,cur;
-        cur=s.charAt(indexFro+1);
-        int tempfro=0,tempNex=0;
+        boolean isCenter= (indexFro + 1) == (indexNex - 1);
+        int indexCur=indexFro+1;
+        cur=s.charAt(indexCur);
+
         while(indexFro>0&&indexNex<s.length()-1) {
             front=s.charAt(--indexFro);
             next=s.charAt(++indexNex);
@@ -108,17 +114,42 @@ public class _5_longestPalindrome {
                 break;
             }
         }
+        if(!isCenter) {
+            lenAndPos[0]=curLen;
+            lenAndPos[1]=startPos;
+            return lenAndPos;
+        }
         if(indexNex!=s.length()-1) {
-            while(indexNex<s.length()-1) {
-                if(s.charAt(++indexNex)==cur) {
-                    curLen++;
+            boolean isSingleStr=true;
+            for(int tempNex=indexNex;tempNex>indexCur;tempNex--) {
+                if(s.charAt(tempNex)!=cur) {
+                    isSingleStr=false;
                 }
             }
-        }else {
-            while(indexFro>0) {
-                if(s.charAt(--indexFro)==cur) {
+            if (isSingleStr) {
+            while(indexNex<s.length()-1) {
+                if (s.charAt(++indexNex) == cur && s.charAt(--indexNex) == s.charAt(++indexNex)) {
                     curLen++;
-                    startPos=indexFro;
+                } else {
+                    break;
+                }
+            }
+            }
+        }else if(indexFro!=0) {
+            boolean isSingleStr=true;
+            for(int tempFro=indexFro;tempFro<indexCur;tempFro++) {
+                if(s.charAt(tempFro)!=cur) {
+                    isSingleStr=false;
+                }
+            }
+            if(isSingleStr) {
+                while (indexFro > 0) {
+                    if (s.charAt(--indexFro) == cur) {
+                        curLen++;
+                        startPos = indexFro;
+                    } else {
+                        break;
+                    }
                 }
             }
         }
@@ -128,7 +159,7 @@ public class _5_longestPalindrome {
     }
 
     public static void main(String[] args) {
-        String test="ccccca";
+        String test="aaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyzzzzzzzzzzyyyyyyyyyyxxxxxxxxxxwwwwwwwwwwvvvvvvvvvvuuuuuuuuuuttttttttttssssssssssrrrrrrrrrrqqqqqqqqqqppppppppppoooooooooonnnnnnnnnnmmmmmmmmmmllllllllllkkkkkkkkkkjjjjjjjjjjiiiiiiiiiihhhhhhhhhhggggggggggffffffffffeeeeeeeeeeddddddddddccccccccccbbbbbbbbbbaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyzzzzzzzzzzyyyyyyyyyyxxxxxxxxxxwwwwwwwwwwvvvvvvvvvvuuuuuuuuuuttttttttttssssssssssrrrrrrrrrrqqqqqqqqqqppppppppppoooooooooonnnnnnnnnnmmmmmmmmmmllllllllllkkkkkkkkkkjjjjjjjjjjiiiiiiiiiihhhhhhhhhhggggggggggffffffffffeeeeeeeeeeddddddddddccccccccccbbbbbbbbbbaaaa";
         _5_longestPalindrome longestPalindrome=new _5_longestPalindrome();
         char[] result = new char[test.length()];
         System.out.println(longestPalindrome.longestPalindrome(test));
