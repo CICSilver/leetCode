0b90998501cdffaa7f435e88f48a1191669b757c
CICSilver <1038516368@qq.com>
2019-06-28 08:14:10 +0800

    添加课设用注释

--- src/sparseMatrix.java
+++ src/sparseMatrix.java
@@ -1,6 +1,16 @@
+/**
+ * 默认该稀疏矩阵仅存储int型数据
+ * 该结构以数组下标存储行信息，以链表结点属性存储列信息与元素信息
+ * 链表头结点存储于行数组中
+ */
 public class sparseMatrix {
+    /**
+     * 列结点定义
+     */
     class colNode{
+        //列属性
         int col;
+        //值
         int val;
         colNode next;
         colNode(int col,int val){
@@ -8,30 +18,34 @@ public class sparseMatrix {
             this.val=val;
         }
     }
-
     /**
      * 矩阵转化为按行单链表
      * @param matrix 输入矩阵
      * @return 按行的单链表头结点数组
      */
     private colNode[] toArray(int[][] matrix) {
-        colNode[] head=new colNode[matrix.length];
-        for(int i=0;i<matrix.length;i++) {
-            for(int j=0;j<matrix[0].length;j++) {
+        int rowLen=matrix.length,colLen=matrix[0].length;
+        //按行头结点数组定义
+        colNode[] row=new colNode[matrix.length];
+        //构造初始的头结点数组
+        for(int i=0;i<rowLen;i++) {
+            for(int j=0;j<colLen;j++) {
+                //在该行找到第一个不为0的元素并生成其对应的结点
                 if(matrix[i][j]!=0) {
-                    head[i]=new colNode(j,matrix[i][j]);
+                    row[i]=new colNode(j,matrix[i][j]);
                     break;
                 }
-                if(j==matrix[0].length-1&&matrix[i][j]==0) {
-                    head[i]=new colNode(j,0);
+                //处理整行都未空情况，生成一个值为0的结点
+                if(j==colLen-1&&matrix[i][j]==0) {
+                    row[i]=new colNode(j,0);
                 }
             }
         }
-
+        //逐行连接后续节点
         for(int i=0;i<matrix.length;i++) {
-            linkCols(head[i],matrix[i].length,matrix[i]);
+            linkCols(row[i],matrix[i].length,matrix[i]);
         }
-        return head;
+        return row;
     }
 
     /**
@@ -39,18 +53,25 @@ public class sparseMatrix {
      * @param head 这一行第一个不为0的元素，即指定行的头结点
      * @param len 行长度
      * @param rowOfMatrix 指定行元素序列
-     * @return 最后一个部位0的元素结点
+     * @return 最后一个不为0的元素结点
      */
-    private colNode linkCols(colNode head,int len,int[] rowOfMatrix) {
+    private void linkCols(colNode head,int len,int[] rowOfMatrix) {
         int curCol=head.col;
-        if(head.col!=len-1) {
+        //判断该结点是否为该行最后一个元素，不是就列指针后移一位
+        if(head.col == len-1) {
+            return;
+        }else {
             curCol+=1;
         }
+        //判断当前行是否存在数据
         if(head.val==0) {
-            return head;
+            return;
         }
+
+        //定义当前结点指针
         colNode curNode=head;
         int val;
+        //遍历当前行，找到头结点之后每个不为0的元素，每找到一个就连接一个
         for(;curCol<len;curCol++) {
             val=rowOfMatrix[curCol];
             if(val!=0) {
@@ -58,7 +79,6 @@ public class sparseMatrix {
                 curNode=curNode.next;
             }
         }
-        return curNode;
     }
 
     /**
@@ -100,23 +120,8 @@ public class sparseMatrix {
                 {0,0,0,0,0,0,0,0},
                 {0,21,0,34,0,0,0,75},
                 {0,0,0,0,0,0,0,0}};
-        int[][] matrix2={
-                {1,0,0,0,0,-5,0,0},
-                {11,0,43,0,0,0,77,0},
-                {0,0,0,0,0,0,0,0},
-                {0,0,0,0,0,51,0,0},
-                {0,0,0,0,0,0,0,0},
-                {0,-21,0,-34,0,0,0,0},
-                {0,0,0,0,0,0,0,0}};
         sparseMatrix sparseMatrix=new sparseMatrix();
-        colNode[] test=sparseMatrix.toArray(matrix2);
-        int[][] result=sparseMatrix.addTwoMatrix(matrix,matrix2);
-        for (int[] ints : result) {
-            for (int j = 0; j < result[0].length; j++) {
-                System.out.print(ints[j] + ",");
-            }
-            System.out.println();
-        }
+        colNode[] res=sparseMatrix.toArray(matrix);
     }
 
 }
